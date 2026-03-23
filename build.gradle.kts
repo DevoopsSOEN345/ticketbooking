30d2971f8f6ca6a40e734f1bf2c26f5b52f21dab
JonathanFrantzeskos <jonathan2.frantzeskos@gmail.com>
2026-03-23 13:11:39 -0400

    sonar test

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ sonarqube {
     properties {
         property("sonar.sources", "app/src/main/java")
         property("sonar.tests", "app/src/test/java")
+        property("sonar.java.binaries", "app/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes")
     }
 }
 
